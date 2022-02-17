8f8f4215f248e519e3633f1fa14045a25e1f377f
fernandoguim <39016558+fernandoguim@users.noreply.github.com>
2022-02-17 00:51:55 -0300

    https://github.com/fernandoguim/Academia-Tecnica-Capgemini-2022

--- src/questao1.js
+++ src/questao1.js
@@ -11,13 +11,13 @@ public class Main {
     System.out.print("Digite a quantidade de vezes: ");
     int n = sc.nextInt(); 
 
-for (int i = 0; i < n; i++) {
-degraus.add(" ".repeat(n - i) + "*".repeat(i + 1));
-}
-for (String d : degraus ) {
-System.out.println(d);
-}
- 
+    for (int i = 0; i < n; i++) {
+    degraus.add(" ".repeat(n - i) + "*".repeat(i + 1));
+    }
 
+    for (String d : degraus ) {
+    System.out.println(d);
+    }
+ 
   }
 }
