daff5f008a91d398782aed96797f2db6daa82b8a
fernandoguim <39016558+fernandoguim@users.noreply.github.com>
2022-02-17 00:50:31 -0300

    https://github.com/fernandoguim/Academia-Tecnica-Capgemini-2022

---
+++ src/questao1.js
@@ -0,0 +1,23 @@
+// Questaão 1 do desafio da Academia Capgemini // Escreva um algoritmo que mostre na tela uma escada de tamanho n utilizando o caractere * e espaços. A base e altura da escada devem ser iguais ao valor de n. A última linha não deve conter nenhum espaço.
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner (System.in);
+    ArrayList<String> degraus = new ArrayList<>(); 
+
+    System.out.print("Digite a quantidade de vezes: ");
+    int n = sc.nextInt(); 
+
+for (int i = 0; i < n; i++) {
+degraus.add(" ".repeat(n - i) + "*".repeat(i + 1));
+}
+for (String d : degraus ) {
+System.out.println(d);
+}
+ 
+
+  }
+}
